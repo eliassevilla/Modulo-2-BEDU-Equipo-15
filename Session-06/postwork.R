8aea763cc4941de737748d41b101c975cc97ee66
Antonio <tonnycav@hotmail.com>
2021-02-01 13:45:12 -0600

    postwork6 completo

--- Session-06/postwork.R
+++ Session-06/postwork.R
@@ -1,4 +1,4 @@
-install.packages('lubridate')
+#install.packages('lubridate')
 library(dplyr)
 library(lubridate)
 info <- read.csv(url("https://raw.githubusercontent.com/beduExpert/Programacion-con-R-Santander/master/Sesion-06/Postwork/match.data.csv"), header = TRUE, sep = ',')
@@ -10,3 +10,22 @@ info2 <- info2 %>%
 info2$date <- as.Date(info2$date , format = "%Y-%m-%d")
 head(info2)
 str(info2)
+
+info3 <- info2 %>% group_by(month=floor_date(date, "month")) %>%
+  summarize(
+    goalspermonth=sum(sumagoles),
+    games = n(),
+    averagegoals = sum(sumagoles)/n()
+  )
+
+info2
+str(info3)
+head(info3)
+
+goles.ts <- ts(info3[, 4], start = c(2010, 8), frequency = 12)
+print(goles.ts)
+
+plot(goles.ts,
+     main = "Promedio de goles por partido", 
+     xlab = "Tiempo",
+     sub = "Agosto de 2010 - Diciembre de 2018")
