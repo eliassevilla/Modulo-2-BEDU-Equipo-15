d50f78481decf70f468e22e8f9c2279d07925bfa
dapariciom <dapariciom@outlook.com>
2021-02-01 23:37:03 -0600

    avance del proyecto

--- Project/Project.R
+++ Project/Project.R
@@ -1,25 +1,33 @@
 
 ########################################################################################################
-#####                                 Instalar y cargar librerias                                   ####
+#####                                   Instalar librerías                                          ####
 ########################################################################################################
 
 
 install.packages("lubridate")
-
-library(dbplyr)
-library(pool)
-library(lubridate)
+install.packages("dplyr")
+install.packages("pool")
+install.packages("ggplot2")
 
 
 ########################################################################################################
-#####                                 Limpiar el cvs princial                                       ####
+#####                                   Utilizar librerías                                          ####
+########################################################################################################
+
+
+library(dplyr)
+library(ggplot2)
+
+
+########################################################################################################
+#####                                 Limpiar el CVS princial                                       ####
 ########################################################################################################
 
 
 data <- read.csv("indicadores.csv", fileEncoding="UTF-8-BOM")
-str(data)
-data <- select(data, General:Energeticos) 
-data <- mutate(data, Periodo= seq(ymd("1970-1-1"), ymd("2020-12-1"), by = "months"))
+str(data)                                                                                    ###################
+data <- select(data, General:Energeticos)                                                    ## Hacer una vez ##
+data <- mutate(data, Periodo= seq(ymd("1970-1-1"), ymd("2020-12-1"), by = "months"))         ###################
 write.csv(data, "Indice-nacional-de-precios-al-consumidor.csv", row.names = FALSE)
 
 
@@ -57,7 +65,19 @@ tail(indices.2005)
 
 
 ########################################################################################################
-#####                    Construir data frame por cada uno de los elmentos                          ####
+#####                      Informacion general relevante de 2005 a 2020                             ####
+########################################################################################################
+
+
+indices.2005.maxgeneral <- which.max(indices.2005$General)
+paste("En la fecha", indices.2005$Periodo[indices.2005.maxgeneral]," se presento la mayor inflación registrada de 2005 a 2020 con un" , indices.2005$General[indices.2005.maxgeneral], "%")
+
+indices.2005.mingeneral <- which.min(indices.2005$General)
+paste("En la fecha", indices.2005$Periodo[indices.2005.mingeneral]," se presento la menor inflación registrada de 2005 a 2020 con un" , indices.2005$General[indices.2005.mingeneral], "%")
+
+
+########################################################################################################
+#####                    Construir data frame por cada uno de los elementos                         ####
 ########################################################################################################
 
 
@@ -68,6 +88,33 @@ indices.2005.servicios <- data.frame(x = indices.2005$Periodo, y = indices.2005$
 indices.2005.nosubyacente <- data.frame(x = indices.2005$Periodo, y = indices.2005$Nosubyacente, group = rep("Nosubyacente", nrow(indices.2005)))
 indices.2005.agropecuarios <- data.frame(x = indices.2005$Periodo, y = indices.2005$Agropecuarios, group = rep("Agropecuarios", nrow(indices.2005)))
 indices.2005.energeticos <- data.frame(x = indices.2005$Periodo, y = indices.2005$Energeticos, group = rep("Energeticos", nrow(indices.2005)))
+
+indices.2005.lista <- list(indices.2005.general, 
+                           indices.2005.subyacente,
+                           indices.2005.mercancias,
+                           indices.2005.servicios,
+                           indices.2005.nosubyacente,
+                           indices.2005.agropecuarios,
+                           indices.2005.energeticos)
+
+indices.2005.groupdata <- do.call(rbind,indices.2005.lista)
+indices.2005.groupdata
+
+
+########################################################################################################
+#####                         Informacion relevante de 2005 a 2020                                  ####
+########################################################################################################
+
+
+indices.2005.maxgroup <- which.max(indices.2005.groupdata$y)
+paste("En la fecha", indices.2005.groupdata$x[indices.2005.maxgroup], "la categoría de", 
+      indices.2005.groupdata$group[indices.2005.maxgroup],
+      "se presento la mayor inflación registrada de 2005 a 2020 con el" , indices.2005.groupdata$y[indices.2005.maxgroup], "%")
+
+indices.2005.mingroup <- which.min(indices.2005.groupdata$y)
+paste("En la fecha", indices.2005.groupdata$x[indices.2005.mingroup], "la categoría de", 
+      indices.2005.groupdata$group[indices.2005.mingroup],
+      "se presento la menor inflación registrada de 2005 a 2020 con el" , indices.2005.groupdata$y[indices.2005.mingroup], "%")
 
 
 ########################################################################################################
@@ -85,22 +132,101 @@ indices.2005.subgeneral.plot <- ggplot( rbind(indices.2005.general,
 indices.2005.subgeneral.plot
 
 
+ts.plot(cbind(Trend, Trend*Seasonal), , main = "Datos de Producción de Electricidad", 
+        ylab = "Producción de electricidad", lty = 1:2,
+        sub = "Tendencia con efectos estacionales multiplicativos sobrepuestos")
+
+
 indices.2005.total.plot <- ggplot(rbind(indices.2005.general,
-                                  indices.2005.subyacente,
                                   indices.2005.mercancias,
-                                  indices.2005.servicios,
-                                  indices.2005.nosubyacente,
-                                  indices.2005.agropecuarios,
-                                  indices.2005.energeticos),
+                                  indices.2005.servicios),
                             aes(x, y, col = group)) + geom_line()
 
 indices.2005.total.plot
 
 
+indices.2005.total.plot <- ggplot(rbind(indices.2005.general,
+                                        indices.2005.agropecuarios,
+                                        indices.2005.energeticos),
+                                  aes(x, y, col = group)) + geom_line()
+
+indices.2005.total.plot
+
+
 ########################################################################################################
-#####                            Graficar la informacion de indices                                 ####
+#####                                Boxplot de los indices                                         ####
+########################################################################################################
+
+
+ggplot(indices.2005.groupdata, aes(x = group, y = y, fill = group)) + geom_boxplot() +
+  ggtitle("Boxplots de tipo de inflaciones") +
+  xlab("Categorías") +
+  ylab("Inflación")
+
+########################################################################################################
+#####                                      Regresion Lineal                                         ####
 ########################################################################################################
 
 modelo.general <- lm(indices.2005$General ~ indices.2005$Subyacente + indices.2005$Nosubyacente)
 summary(modelo.general)
+
+modelo.componentes <- lm(indices.2005$General ~ indices.2005$Mercancias + indices.2005$Servicios + indices.2005$Agropecuarios + indices.2005$Energeticos)
+summary(modelo.componentes)
+
+
+########################################################################################################
+#####                                      Serie de tiempo                                          ####
+########################################################################################################
+
+
+indices.2005.general.ts <- ts(indices.2005$General, start = 2005, freq = 12)
+
+plot(indices.2005.general.ts, 
+     main = "Inflación en México", 
+     xlab = "Tiempo",
+     ylab = "Inflación",
+     sub = "Enero de 2005 - Diciembre de 2020")
+
+
+########################################################################################################
+#####                                      Modelo Aditivo                                           ####
+########################################################################################################
+
+
+indices.2005.general.ts.aditivo <- decompose(indices.2005.general.ts)
+
+plot(indices.2005.general.ts.aditivo, xlab = "Tiempo", 
+     sub = "Descomposición de los datos de inflacion")
+
+Tendencia <- indices.2005.general.ts.aditivo$trend
+Estacionalidad <- indices.2005.general.ts.aditivo$seasonal
+Aleatorio <- indices.2005.general.ts.aditivo$random
+
+ts.plot(cbind(Tendencia, Tendencia + Estacionalidad), 
+        xlab = "Tiempo", main = "Inflación en México", 
+        ylab = "Inflación", lty = 1:2)
+
+
+########################################################################################################
+#####                                    Modelo Multiplicativo                                      ####
+########################################################################################################
+
+
+indices.2005.general.ts.multiplicativo <- decompose(indices.2005.general.ts, type = "mult")
+
+plot(indices.2005.general.ts.multiplicativo, xlab = "Tiempo", 
+     sub = "Descomposición de los datos de inflación")
+
+Trend <- indices.2005.general.ts.multiplicativo$trend
+Seasonal <- indices.2005.general.ts.multiplicativo$seasonal
+Random <- indices.2005.general.ts.multiplicativo$random
+
+ts.plot(cbind(Trend, Trend*Seasonal), xlab = "Tiempo", main = "Inflación en México", 
+        ylab = "Inflación", lty = 1:2)
+
+
+
+
+
+
 
--- Session-04/Poswork_04.R
+++ Session-04/Poswork_04.R
@@ -1,58 +1,42 @@
+install.packages("tidyvers")
 
 library(dplyr)
 library(reshape2)
 library(ggplot2)
+library(tidyverse)
 
 data1718 <- read.csv("https://www.football-data.co.uk/mmz4281/1718/SP1.csv")
 data1819 <- read.csv("https://www.football-data.co.uk/mmz4281/1819/SP1.csv")
 data1920 <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
 
-View(data1718)
-View(data1819)
-View(data1920)
-
 lista <- list(data1718, data1819, data1920)
 lista <- lapply(lista, select, Date, HomeTeam, AwayTeam, FTHG, FTAG, FTR)
-
-head(lista[[1]]); head(lista[[2]]); head(lista[[3]]);
 
 lista[[1]] <- mutate(lista[[1]], Date = as.Date(Date, "%d/%m/%y"))
 lista[[2]] <- mutate(lista[[2]], Date = as.Date(Date, "%d/%m/%Y"))
 lista[[3]] <- mutate(lista[[3]], Date = as.Date(Date, "%d/%m/%Y"))
 
-head(lista[[1]]); head(lista[[2]]); head(lista[[3]]);
-
 data <- do.call(rbind, lista)
-
-head(data)
-View(data)
 
 x <- data$FTHG
 y <- data$FTAG
 
-(tablex <- table(x))
-(tablex <- prop.table(tablex))
-str(tablex)
+tablex <- prop.table(table(x))
+tabley <- prop.table(table(y))
+tablexy <- prop.table(table(x,y))
 
-#tablex <- as.data.frame(tablex)
-#str(tablex)
+cocientes <- sweep(tablexy, MARGIN = 1, 1/tablex, `*`)
+cocientes <- sweep(cocientes, MARGIN = 2, 1/tabley, `*`)
 
-(tabley <- table(y))
-(tabley <- prop.table(tabley))
-str(tabley)
+newx <- sample(x, replace = TRUE)
+bootstrapx <- prop.table(table(newx))
 
-#tabley <- as.data.frame(tabley)
-#str(tabley)
+newy <- sample(y, replace = TRUE)
+bootstrapy <- prop.table(table(newy))
 
-(tablexy <- table(x,y))
-(tablexy <- prop.table(tablexy))
-str(tablexy)
+bootstrapxy <- prop.table(table(newx, newy))
 
-#tablexy <- melt(tablexy)
-#str(tablexy)
-
-
-
-
-
+bootstrap.cocientes <- sweep(bootstrapxy, MARGIN = 2, 1/bootstrapy, `*`)
+bootstrap.cocientes <- sweep(bootstrap.cocientes, MARGIN = 1, 1/bootstrapx, `*`)
+bootstrap.cocientes
 
